2139a1caa129856f55338e087949c29452f9bf98
aisy <aisymuhammad@gmail.com>
2019-10-08 17:30:34 +0700

    add login page in router.js

--- src/configs/router.js
+++ src/configs/router.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 // Load components page
+import Login from '../pages/Login'
 import Home from '../pages/Home'
 
 const Router = () => {
   return (
     <Switch>
-      <Route path="/" component={Home} />
+      <Route path="/" component={Login} />
       <Route path="/Home" component={Home} />
     </Switch>
   )
