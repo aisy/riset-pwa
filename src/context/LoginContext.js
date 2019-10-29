614c0fd96114a875846ee3651cd7f409829a4064
aisy <aisymuhammad@gmail.com>
2019-10-29 16:26:27 +0700

    change context

--- src/context/LoginContext.js
+++ src/context/LoginContext.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState, createContext } from 'react'
 
-const LoginContext = React.createContext({});
+const LoginContext = createContext({});
 
-export const LoginProvider = LoginContext.Provider;
+const LoginProvider = (props) => {
+  const { children } = props;
+
+  // const [user, setUser] = useState([]);
+
+  // const setLogin = dataLogin => {
+
+  // }
+
+  const loginCon = {
+    user,
+    setUser,
+  }
+
+  return (
+    <LoginContext.Provider value={loginCon}>
+      {children}
+    </LoginContext.Provider>
+  )
+}
+
+// export const LoginProvider = LoginContext.Provider;
 export const LoginConsumer = LoginContext.Consumer;
 
 export default LoginContext
